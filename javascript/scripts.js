3657b5c7dbcfee9fda0e95e4f952f53b65a13cdd
willyowi <wilsonowino1@gmail.com>
2019-05-29 18:35:19 +0300

    add html file and link

---
+++ javascript/scripts.js
@@ -0,0 +1,37 @@
+var add = function(one,two){
+return (one + two);
+};
+var one =parseInt(prompt("Enter a number"));
+alert(one);
+var two =parseInt(prompt("Enter another number"));
+alert(add(one,two));
+var add = function(one,two){
+return (one * two);
+};
+var one =parseInt(prompt("Enter a number"));
+alert(one);
+var two =parseInt(prompt("Enter another number"));
+alert(add(one,two));
+
+var add = function(one,two){
+return (one - two);
+};
+var one =parseInt(prompt("Enter a number"));
+alert(one);
+var two =parseInt(prompt("Enter another number"));
+alert(add(one,two));
+var add = function(one,two){
+return (one / two);
+};
+var one =parseInt(prompt("Enter a number"));
+alert(one);
+var two =parseInt(prompt("Enter another number"));
+alert(add(one,two));
+var add = function(one,two){
+return (one % two);
+};
+var one =parseInt(prompt("Enter a number"));
+alert(one);
+var two =parseInt(prompt("Enter another number"));
+alert(add(one,two));
+
